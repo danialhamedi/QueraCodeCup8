f244530374d4bdedb6b92cf036d3e9d6372fd81d
Danial Hamedi <danial@datachef.co>
2024-02-23 19:33:36 +0330

    /

--- sumOfBrackets.py
+++
@@ -1,9 +0,0 @@
-tests = int(input())
-for _ in range(tests):
-	n = int(input())
-	x = float(input())
-	ans = n*int(x)
-	remainToTwo = int(x)+1 - x
-	secondround = int(remainToTwo*n + 1) + 1 
-	secondround = n - secondround
-	print(ans + secondround+ 1)
